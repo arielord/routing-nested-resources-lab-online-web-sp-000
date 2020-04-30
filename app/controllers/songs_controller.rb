4dbbf861499c535dfcc4f636d0df023ec889f138
Ariel <arielord@buffalo.edu>
2020-04-30 02:44:05 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,7 @@ class SongsController < ApplicationController
       if artist
         @songs = artist.songs
       else
+        flash[:notice] = "Artist not found"
         redirect_to artists_path
       end
     else
